381888c9225fbe347d71fa8f0d89b88256b72e4c
Sreedhar375 <sreedharreddybollup@gmail.com>
2025-06-06 14:51:38 +0530

    Add files via upload

---
+++ Bubblesort.java
@@ -0,0 +1,36 @@
+package f;
+
+import java.util.Arrays;
+
+public class Bubblesort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] numbers= {5,2,4,1,3};
+		//Bubble sort logic
+		for(int i=0;i<numbers.length -1;i++) {
+			for(int j=0;j<numbers.length -1;j++) {
+				if(numbers[j]>numbers[j+1]) {
+					//swapping
+					int temp=numbers[j];
+					numbers[j]=numbers[j+1];
+					numbers[j+1]=temp;
+					
+				}
+			}
+		}
+//print the sorted numbers
+		System.out.println("Sorted List");
+		for( int num:numbers) {
+			System.out.println(num);
+		}
+		//to sort the array in the same line 
+		System.out.println(Arrays.toString(numbers));
+	}
+
+}
+/* Student MAnagement (OOP + ArrayList)
+Task: Create a student name,  age and grade
+Add 5 students to ArrayList
+Print the ArrayList with grade above 80*/
+
